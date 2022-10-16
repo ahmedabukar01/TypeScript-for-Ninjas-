5ed1aaee04d3db550e4e639db162b8554dd35b7a
ahmed <ahmedlearn01@gmail.com>
2022-10-16 11:48:53 +0300

    explicit types

--- sandbox.ts
+++ sandbox.ts
@@ -9,3 +9,35 @@ console.log(inputs);
 inputs.forEach(input => {
     console.log(input)
 })
+
+// explicit Types 
+let Actor: string;
+let age: number;
+let isLoggedIn: boolean;
+
+// arrays
+const ninja: string[] = [];
+ninja.push('33');
+
+// Union types
+let moreNinjas: (string | number | boolean)[] = [];
+moreNinjas.push(33);
+moreNinjas.push('more');
+moreNinjas.push(false);
+
+let uid: string | number ;
+
+// objects
+
+
+let ninjaOne: {
+    name: string,
+    age: number,
+    beltColor: string
+}
+
+ninjaOne = {
+    name: 'mario',
+    age: 30,
+    beltColor: 'black'
+}
