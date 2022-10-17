01db6bfc5b65a3a7622845dc867297674b3fd0af
ahmed <ahmedlearn01@gmail.com>
2022-10-17 11:05:03 +0300

    jumping into html elements

--- Project/public/app.js
+++ Project/public/app.js
@@ -1,2 +1,6 @@
 "use strict";
-console.log('here i am');
+const Anchor = document.querySelector('a');
+// giving ! after the document means, i am sure that it will not return null.
+const Form = document.querySelector('.new-item-form');
+console.log(Anchor.href);
+console.log(Form.children);
--- Project/src/app.ts
+++ Project/src/app.ts
@@ -1,1 +1,6 @@
-console.log('here i am')
+const Anchor = document.querySelector('a')!;
+// giving ! after the document means, i am sure that it will not return null.
+const Form = document.querySelector('.new-item-form') as HTMLFormElement;
+
+console.log(Anchor.href);
+console.log(Form.children);
