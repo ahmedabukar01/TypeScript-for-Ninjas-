a9cc68e516767a6ea9966980a6467f958e021ad7
ahmed <ahmedlearn01@gmail.com>
2022-10-17 11:32:38 +0300

    some changes

--- Project/public/app.js
+++ Project/public/app.js
@@ -1,6 +1,14 @@
 "use strict";
-const Anchor = document.querySelector('a');
+const anchor = document.querySelector('a');
 // giving ! after the document means, i am sure that it will not return null.
-const Form = document.querySelector('.new-item-form');
-console.log(Anchor.href);
-console.log(Form.children);
+const form = document.querySelector('.new-item-form');
+const type = document.querySelector('#type');
+const toFrom = document.querySelector('#toFrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+// console.log(anchor.href);
+// console.log(form.children);
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
+});
--- Project/src/app.ts
+++ Project/src/app.ts
@@ -1,6 +1,21 @@
-const Anchor = document.querySelector('a')!;
+const anchor = document.querySelector('a')!;
 // giving ! after the document means, i am sure that it will not return null.
-const Form = document.querySelector('.new-item-form') as HTMLFormElement;
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+const type = document.querySelector('#type') as HTMLSelectElement;
+const toFrom = document.querySelector('#toFrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
 
-console.log(Anchor.href);
-console.log(Form.children);
+// console.log(anchor.href);
+// console.log(form.children);
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    console.log(
+        type.value,
+        toFrom.value,
+        details.value,
+        amount.valueAsNumber
+    )
+})
+
